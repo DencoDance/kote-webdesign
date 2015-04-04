727b74fe89a6797255708e54a61b7e22bde5fb3f
DencoDance <di.nekto@gmail.com>
2015-04-04 15:37:59 +0300

    js changes and parked changes

--- js/common.js
+++ js/common.js
@@ -2,19 +2,13 @@
  * Created by DencoDance on 01.03.2015.
  */
 $(function () {
+    switchLogo(0);
     $(window).scroll(function () {
         var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
         switchLogo(scrollTop);
     });
-});
-function switchLogo(pos) {
-    if (pos >= $('#big-logo').height() || pos > 200) {
-        if (document.getElementById('big-logo')) {
-            document.getElementById('small-logo').style.opacity = '1';
-            document.getElementById('big-logo').style.opacity = '0';
-            document.getElementById('bar').style.backgroundColor = 'rgba(0,0,0, 0.6)';
-            $('#top-shadow').hide();
-        }else{
+});function switchLogo(pos) {
+    if($('#big-logo').length == 0){
             document.getElementById('small-logo').style.opacity = '1';
             $('#top-shadow').hide();
             $('#user').attr('src', 'img/user-black.png');
@@ -24,24 +18,60 @@ function switchLogo(pos) {
             document.getElementById('bar').style.backgroundColor = 'rgba(255,255,255, 1)';
             $('#username').addClass('username-other');
             $('nav').addClass('nav-other');
-        }
     }
-    if(pos<100) {
-        if (document.getElementById('big-logo')) {
+    else{
+        if (pos >= $('#big-logo').height() || pos > 200) {
+            document.getElementById('small-logo').style.opacity = '1';
+            document.getElementById('big-logo').style.opacity = '0';
+            document.getElementById('bar').style.backgroundColor = 'rgba(0,0,0, 0.6)';
+            $('#top-shadow').hide();
+        }
+        else{
             document.getElementById('bar').style.backgroundColor = 'transparent';
             document.getElementById('small-logo').style.opacity = '0';
             document.getElementById('big-logo').style.opacity = '1';
             $('#top-shadow').show();
-        }else{
-            $('#top-shadow').show();
-            $('#user').attr('src', 'img/user.png');
-            $('#Searching').attr('src', 'img/Searching.png');
-            $('.separator').attr('src', 'img/separator.png');
-            $('#small-logo').attr('src', 'img/small-logo.png');
-            document.getElementById('bar').style.backgroundColor = 'transparent';
-            $('#username').removeClass('username-other');
-            $('nav').removeClass('nav-other');
         }
-
     }
 }
+//function switchLogo(pos) {
+//    if (pos >= $('#big-logo').height() || pos > 200) {
+//        if (document.getElementById('big-logo')) {
+//            document.getElementById('small-logo').style.opacity = '1';
+//            document.getElementById('big-logo').style.opacity = '0';
+//            document.getElementById('bar').style.backgroundColor = 'rgba(0,0,0, 0.6)';
+//            $('#top-shadow').hide();
+//        }else{
+//            document.getElementById('small-logo').style.opacity = '1';
+//            $('#top-shadow').hide();
+//            $('#user').attr('src', 'img/user-black.png');
+//            $('#Searching').attr('src', 'img/Searching-black.png');
+//            $('.separator').attr('src', 'img/separator-black.png');
+//            $('#small-logo').attr('src', 'img/small-logo-red.png');
+//            document.getElementById('bar').style.backgroundColor = 'rgba(255,255,255, 1)';
+//            $('#username').addClass('username-other');
+//            $('nav').addClass('nav-other');
+//        }
+//    }
+//    if(pos<100 && pos!=0) {
+//        if (document.getElementById('big-logo')) {
+//            document.getElementById('bar').style.backgroundColor = 'transparent';
+//            document.getElementById('small-logo').style.opacity = '0';
+//            document.getElementById('big-logo').style.opacity = '1';
+//            $('#top-shadow').show();
+//        }else{
+//            $('#top-shadow').show();
+//            $('#user').attr('src', 'img/user.png');
+//            $('#Searching').attr('src', 'img/Searching.png');
+//            $('.separator').attr('src', 'img/separator.png');
+//            $('#small-logo').attr('src', 'img/small-logo.png');
+//            document.getElementById('bar').style.backgroundColor = 'transparent';
+//            $('#username').removeClass('username-other');
+//            $('nav').removeClass('nav-other');
+//        }
+//
+//    }
+//    if(pos == 0){
+//        document.getElementById('small-logo').style.opacity = '0';
+//    }
+//}
